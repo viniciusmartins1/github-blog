d49095946420beec0f3e25473aa2508f9710561a
Vinicius Martins <vm3090474@gmail.com>
2023-05-17 22:49:32 -0300

    feat: configure the main styles of the project

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  border-style, input-security, textarea, button {
+    font: 400 1rem 'Nunito', sans-serif;
+  }
+
+`;
