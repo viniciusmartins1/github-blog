373d5b9da800163b1d37c947a3e502dc8dbd76b6
Vinicius Martins <vm3090474@gmail.com>
2023-06-15 22:35:54 -0300

    feat: add params in the post router

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,7 +7,7 @@ export function Router() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/post" element={<Post />} />
+        <Route path="/post/:id" element={<Post />} />
       </Routes>
     </>
   );
